c10f8235467221fc740a3b176fdd6e184fd85fd1
Neil Brough <2555723B@student.gla.ac.uk>
2022-02-10 18:46:00 +0000

    Final commit chapter 10 complete

--- rango/views.py
+++ rango/views.py
@@ -5,28 +5,62 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
 
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
 
+    visitor_cookie_handler(request)
     # Construct a ditionary to pass to the template engine as its context
     #Note the key boldmessage matches to {{ boldmessage }} in the template
-    context_dict = {}
-    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
-    context_dict['categories'] = category_list
-    context_dict['pages'] = page_list
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
+                    'categories': category_list,
+                    'pages': page_list,
+    }
     
-
 
     # Return a rendered response to send to the clinet.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/index.html', context=context_dict)
-
+    
+    
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by Neil'}
+
+    visitor_cookie_handler(request)
+    context_dict = {'boldmessage': 'This tutorial has been put together by Neil',
+                    'visits': request.session['visits'],
+                    }
+    
+    request.session.set_test_cookie()
+    if request.session.test_cookie_worked():
+        print('TEST COOKIE WORKED!')
+        request.session.delete_test_cookie()
 
     return render(request, 'rango/about.html', context=context_dict)
     
